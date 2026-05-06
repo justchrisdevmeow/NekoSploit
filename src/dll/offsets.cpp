dfd569791b5d2573a0557a685b996fa92ab46347
notchwis <kodenothing@gmail.com>
2026-05-05 19:26:55 -0500

    Create offsets.cpp

---
+++ src/dll/offsets.cpp
@@ -0,0 +1,7 @@
+// Generated automatically by NekoSploit scanner
+// DO NOT EDIT MANUALLY - Run scanner again to update
+
+#define OFFSET_luaL_loadstring 0x00000000
+#define OFFSET_lua_pcall 0x00000000
+#define OFFSET_lua_getglobal 0x00000000
+#define OFFSET_LuaState 0x00000000
