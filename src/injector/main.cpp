3d6ff52248fd9766658ff58e0cd9a0b721879bb3
notchwis <kodenothing@gmail.com>
2026-05-05 19:20:34 -0500

    Update main.cpp

--- src/injector/main.cpp
+++ src/injector/main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
-#include "gui.h"
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nShow) {
+    // Call gui.cpp's main function
+    extern int CreateMainWindow(HINSTANCE, int);
     return CreateMainWindow(hInst, nShow);
 }
